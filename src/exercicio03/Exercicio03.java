11b51c3006c5b094ff87ef47d2a715b2191f2174
Marcos Takao Barbosa <marcostkao@gmail.com>
2020-02-16 18:44:20 -0300

    commit atualização

---
+++ src/exercicio03/Exercicio03.java
@@ -0,0 +1,11 @@
+package exercicio03;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("A arte da guerra","Sun Tzu", 160);
+
+        //livro1.titulo = "O monge e o executivo";
+
+
+    }
+}
